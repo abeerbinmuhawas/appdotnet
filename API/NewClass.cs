e47fa0da2eb48861b5f3c20be3b2fd53d3cd9cbe
Abeer.Muhawas <abeer.muhawas@gmail.com>
2022-10-03 08:33:12 +0300

    end of section 3

---
+++ API/NewClass.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API
+{
+    public class NewClass
+    {
+        public static void main(string[]args){
+            System.Console.WriteLine("hi");
+        }
+    }
+}
